61c9caa74d3e43b2caf2f6f29167269919dc22f8
Georg Schlenkhoff <georg.schlenkhoff@gmail.com>
2016-09-09 18:42:07 +0200

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('whats-app-api', function() {
+  describe('/register', function() {
+    it('register a numer', function() {
+      assert.equal(1, true);
+    });
+  });
+});
